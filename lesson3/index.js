0f72017edd7316b62f459dbd60291c960e61fae0
faridmia <mdfarid7830@gmail.com>
2026-04-18 16:25:32 +0600

    file path

---
+++ lesson3/index.js
@@ -0,0 +1,21 @@
+// const os = require('os');
+
+// console.log('Operating System:', os.platform());
+// console.log('Architecture:', os.arch());
+// console.log('Total Memory:', os.totalmem());
+// console.log('Free Memory:', os.freemem());
+
+const path = require('path');
+
+// console.log('File Name:', path.basename(__filename));
+// console.log('Directory Name:', path.dirname(__dirname));
+// console.log('File Extension:', path.extname(__filename));
+// console.log('Parsed Path:', path.parse(__filename));
+
+// const extension = path.extname("index.html");
+// console.log('File Extension:', extension);
+
+//const joinedPath = path.join(__dirname, 'subfolder', 'file.txt');
+const joinedPath = path.join(__dirname, "/views");""
+console.log('Joined Path:', joinedPath);
+
