18b3a933551e69b0dbdb4ccbbb97409beb99ab50
faridmia <mdfarid7830@gmail.com>
2026-04-18 12:27:20 +0600

    initialized

---
+++ lesson1/index.js
@@ -0,0 +1,4 @@
+const s1 = require('./student');
+
+console.log(s1.getName("John"));
+console.log(s1.getAge(25));
---
+++ lesson1/student.js
@@ -0,0 +1,10 @@
+const getName = (name) => {
+    return `My name is ${name}`;
+}
+
+const getAge = (age) => {
+    return "20";
+}
+
+exports.getName = getName;
+exports.getAge = getAge;
