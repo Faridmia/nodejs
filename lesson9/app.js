db40c27fe81391251aa6d0c802d9a03ed4f11e14
faridmia <mdfarid7830@gmail.com>
2026-04-22 07:46:16 +0600

    different response request

--- lesson9/app.js
+++ lesson9/app.js
@@ -1,5 +1,24 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+    res.end();
+});
+
+app.post('/', (req, res) => {
+    res.send('post request received!');
+    res.end();
+});
+
+app.put('/', (req, res) => {
+    res.send('put request received!');
+    res.end();
+});
+
+app.delete('/', (req, res) => {
+    res.send('delete request received!');
+    res.end();
+});
 
 module.exports = app;
